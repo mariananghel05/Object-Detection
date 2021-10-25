cbc8a0f3c738a15d5b12511b7bc51e0c39bfc774
anghelmarian05 <mariananghel99@gmail.com>
2021-10-25 03:16:27 +0300

    s

---
+++ src/ImageFilter.py
@@ -0,0 +1,10 @@
+
+
+class ImageFilter:
+    def __init__(self):
+        pass
+    
+    def RGBtoGray(self):
+        pass
+    
+#etc
---
+++ src/Screen.py
@@ -0,0 +1,13 @@
+import time
+import cv2
+
+
+class Screen:
+    def __init__(self):
+        pass
+    
+    def screenshot(self):
+        pass
+    
+    def grab(self):
+        pass
